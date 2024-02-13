b1b78bf0abe3ebee3b0e9d479b58f6e9053d75dc
guravsayali25 <155410230+guravsayali25@users.noreply.github.com>
2024-02-13 15:38:18 +0530

    Add files via upload

---
+++ sayalimaven/src/main/java/com/sgm/sayalimaven/App.java
@@ -0,0 +1,28 @@
+package com.sgm.sayalimaven;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+  public static void main(String[] args) {
+	  System.out.println( "Hello hibernate World!" );
+      Configuration cf=new Configuration();
+      cf.configure("hibernate.cfg.xml");
+      cf.addAnnotatedClass(Place.class);
+     
+      SessionFactory sf=cf.buildSessionFactory();
+      Session s1=sf.openSession();
+     
+      Place p1=new Place();
+      p1.setId(3);
+      p1.setName("karad");
+      p1.setFare(50000);
+      s1.beginTransaction();
+      s1.save(p1);
+      s1.getTransaction().commit();
+      s1.close();
+      System.out.println("one data saved in Place Table");
+  }
+}
+
